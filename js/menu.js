14b5d3b621091d4f5079554e3c3b6ace223b3959
Stepan <morgenstep1@gmail.com>
2025-10-16 21:37:50 +0500

    Second Update

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,8 @@
 document.getElementById('menuToggle').addEventListener('click', () => {
+    const menuBtn = document.getElementById('menuToggle')
     const sidebar = document.getElementById('sidebar');
     const main = document.getElementById('main');
     sidebar.classList.toggle('hidden');
     main.classList.toggle('full');
+    menuBtn.classList.toggle('active');
 });
