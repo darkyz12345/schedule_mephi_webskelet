635fe050b24788def09df3f4c811e11f77b0b0df
Stepan <morgenstep1@gmail.com>
2025-10-17 22:44:40 +0500

    Update

--- js/logpassword.js
+++ js/logpassword.js
@@ -1,14 +1,17 @@
+const inputPassword = document.getElementById('password');
+const icon = document.getElementById('showPass');
+
 document.getElementById("showPass").addEventListener('click', () => {
-    const inputPassword = document.getElementById('password');
-    const icon = document.getElementById('showPass');
     if (inputPassword.type == 'password')
     {
         inputPassword.type = "text";
         icon.innerHTML = '<i class="fa-regular fa-eye-slash"></i>'
+        icon.title = 'Скрыть пароль';
     }
     else 
     {
         inputPassword.type = "password";
         icon.innerHTML = '<i class="fa-regular fa-eye"></i>';
+        icon.title = 'Показать пароль';
     }
 })
